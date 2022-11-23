d7bf23d7cc6a5f242dbbee6526265e7fbe7a413f
DavidLapp <davidlapp101@gmail.com>
2022-11-23 13:55:07 -0500

    creates css, js, and landing page file

---
+++ resources/views/landing-page.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+    <div class="landing-page-header">
+        <p>Header</p>
+    </div>
+    <div class="level1">
+        <p>Level1</p>
+    </div>
+    <div class="level2">
+        <p>Level2</p>
+    </div>
+    <div class="level3">
+        <p>Level3</p>
+    </div>
+    <div class="level4">
+    </div>
+    <div class="level5"></div>
+    <div class="level6"></div>
+</body>
+</html>
