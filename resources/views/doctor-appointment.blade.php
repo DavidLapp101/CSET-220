662cdad087e130a6d6b8d2b62c5c4986b47ec099
Fasnacht <jhfasnacht211@stevenscollege.edu>
2022-11-30 12:33:21 -0500

    Created base html pages for doctor appointments, patient search, list of employees, account approval, and roles. Put the routes in web.php

---
+++ resources/views/doctor-appointment.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<div>
+<h1>Doctor's Appointment:</h1>
+</div>
+<div>
+    <form action="">
+        <input type="text" placeholder="Patient ID">
+        <p>Patient Name will Autopopulate Here:</p>
+        <label for="Date">Choose a Date</label>
+        <input type="date" name="Date">
+        <label for="availableDoctors">Doctor:</label>
+        <select name="availableDoctors" id="">
+            <option value=""></option>
+        </select>
+        <input type="submit" value="OK">
+        <a href="">Cancel</a>
+
+    </form>
+</div>
+
+
+
+
+</html>
---
+++ resources/views/employees.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <h1>Employee</h1>
+    <div>
+        <form action="">
+            <select name="employeeSearch">
+                <option value="ID">ID</option>
+                <option value="name">Name</option>
+                <option value="role">Role</option>
+                <option value="salary">Salary</option>
+            </select>
+            <input type="text" placeholder="search">
+        </form>
+    </div>
+    <div>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Role</th>
+                <th>Salary</th>
+            </tr>
+        
+        </table>
+    </div>
+
+    <div>
+        <form action="">
+            <input type="text" placeholder="Employee ID">
+            <input type="number" placeholder="New Salary">
+            <input type="submit" value="OK">
+            <a href="">Cancel</a>
+        </form>
+    </div>
+</html>
