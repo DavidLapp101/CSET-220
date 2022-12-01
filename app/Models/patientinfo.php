4a6c19cbb936c4666b47e257e09000dbbe532fd5
Isaiah-Lam <isaiah@lamison.net>
2022-12-01 13:41:24 -0500

    replace file

---
+++ app/Models/patientinfo.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class patientinfo extends Model
+{
+    use HasFactory;
+
+    protected $table = "patientinfo";
+
+    protected $fillable = [
+        'userID',
+        'familyCode',
+        'emergencyContact',
+        'contactName',
+        'contactRelation',
+        'dateOfBirth',
+        'admissionDate',
+        'balance'
+    ];
+
+}
