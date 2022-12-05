8ab11a230272d16cc44c6bc5c29253b4b267aad4
Fasnacht <jhfasnacht211@stevenscollege.edu>
2022-12-05 13:33:14 -0500

    Added patients list, added funtionality to the Search form

--- resources/views/patient-search.blade.php
+++ resources/views/patient-search.blade.php
@@ -7,9 +7,7 @@
     <h1>Patient Search</h1>
     <div>
         <form action="" class="search-form">
-            <label for="patientSearch">Search By:</label>
-            <div>
-                <select name="patientSearch" class="search-filter">
+                <select id="patientSearch" name="patientSearch" class="search-filter">
                     <option selected="true" value="ID">Patient ID</option>
                     <option value="name">Name</option>
                     <option value="age">Age</option>
@@ -17,13 +15,50 @@
                     <option value="eContactName">Emergency Contact Name</option>
                     <option value="date">Admission Date</option>
                 </select>
-                <input type="text" class="search-input" placeholder="Search">
+                <input type="text" class="search-input" placeholder="Search" oninput="searchPatient(this.value)">
                 <button type="submit" class="search-submit"><ion-icon name="search-outline"></ion-icon></button>
-            </div>
         </form>
+    </div>
+
+    <div>
+        <table>
+            <tr>
+                <th>Patient ID</th>
+                <th>Name</th>
+                <th>age</th>
+                <th>Emergency Contact</th>
+                <th>Emergency Contact Phone Number</th>
+                <th>Admission Date</th>
+            </tr>
+            <?php
+                for($i=0; $i < count($patientList); $i++){
+                    echo '<tr class="patientSearchList">';
+                    echo '<td class="patientSearchList-ID">'. $patientList[$i]['userID']. '</td>';
+                    echo '<td class="patientSearchList-name">'. $patientList[$i]['name']. '</td>';
+                    echo '<td class="patientSearchList-age">'. $patientList[$i]['age']. '</td>';
+                    echo '<td class="patientSearchList-contactNumber">'. $patientList[$i]['emergencyContact']. '</td>';
+                    echo '<td class="patientSearchList-eContactName">'. $patientList[$i]['contactName']. '</td>';
+                    echo '<td class="patientSearchList-date">'. $patientList[$i]['admissionDate']. '</td>';
+                    echo '</tr>';
+                }
+            ?>
+
+
+
+        </table>
+    </div>
         <script>
             // const patients = document.getElementById('mh-patients');
             // patients.selected = 'true';
+            function searchPatient(value) {
+                $(".patientSearchList-" + $("#patientSearch").val()).each(function() {
+                    if ($(this).text().includes(value)) {
+                        $(this).parent().css("display", "block");
+                    }
+                    else {
+                        $(this).parent().css("display", "none");
+                    }
+                });
+            }
         </script>
-    </div>
 @stop
