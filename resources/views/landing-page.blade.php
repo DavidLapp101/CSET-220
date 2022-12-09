942d66ff8e3da94a945d05305cb98ba4f8c5adc6
DavidLapp <davidlapp101@gmail.com>
2022-12-09 13:06:53 -0500

    Patient home table will not show checkbox if medication isn't assigned in regiment

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -160,51 +160,79 @@ if (session_status() === PHP_SESSION_NONE) {
                         $id = $_SESSION["userID"];
                         for($i=0; $i<count($reg); $i++){
                             if(($reg[$i]['patientID']==$id) && $reg[$i]['date']==$date){
+                                //shows doctors name
                                 if(is_null($reg[$i]['doctorName'])==false){
                                     echo"<td>".$reg[$i]['doctorName']."</td>";
                                 }
                                 else if(is_null($reg[$i]['doctorName'])){
                                     echo"<td>NO DOCTOR</td>";
                                 }
-                                if($reg[$i]['docApt']==1){
+
+                                //shows if appoinment was attended
+                                if(is_null($reg[$i]['doctorName'])){
+                                    echo"<td>-</td>";
+                                }
+                                else if($reg[$i]['docApt']==1){
                                     echo'<td><input type="checkbox" checked disabled></td>';
                                 }
                                 else if($reg[$i]['docApt']==0){
                                     echo'<td><input type="checkbox" disabled></td>';
                                 }
-                                else{
-                                    echo '-';
-                                }
+
+                                //shows caretaker Name
                                 echo '<td>'.$reg[$i]['caretakerID'].'</td>';
-                                if($reg[$i]['morningMed']==1){
+
+                                //Checks if morning medication was taken
+                                if(is_null($takeMeds[0]['morningMed'])){
+                                    echo'<td>-</td>';
+                                }
+                                else if($reg[$i]['morningMed']==1){
                                     echo'<td><input type="checkbox" checked disabled></td>';
                                 }
                                 else if($reg[$i]['morningMed']==0){
                                     echo'<td><input type="checkbox" disabled></td>';
                                 }
-                                if($reg[$i]['afternoonMed']==1){
+
+                                //checks if afternoon medication was taken
+                                if(is_null($takeMeds[0]['afternoonMed'])){
+                                    echo'<td>-</td>';
+                                }
+                                elseif($reg[$i]['afternoonMed']==1){
                                     echo'<td><input type="checkbox" checked disabled></td>';
                                 }
                                 else if($reg[$i]['afternoonMed']==0){
                                     echo'<td><input type="checkbox" disabled></td>';
                                 }
-                                if($reg[$i]['eveningMed']==1){
+
+                                //checks if evening medication was taken
+                                if(is_null($takeMeds[0]['eveningMed'])){
+                                    echo'<td>-</td>';
+                                }
+                                else if($reg[$i]['eveningMed']==1){
                                     echo'<td><input type="checkbox" checked disabled></td>';
                                 }
                                 else if($reg[$i]['eveningMed']==0){
                                     echo'<td><input type="checkbox" disabled></td>';
                                 }
+
+                                //Checks if breakfast was eaten
                                 if($reg[$i]['breakfast']==1){
                                     echo'<td><input type="checkbox" checked disabled></td>';
                                 }
                                 else if($reg[$i]['breakfast']==0){
                                     echo'<td><input type="checkbox" disabled></td>';
-                                }if($reg[$i]['lunch']==1){
+                                }
+                                
+                                //checks if lunch was eaten
+                                if($reg[$i]['lunch']==1){
                                     echo'<td><input type="checkbox" checked disabled></td>';
                                 }
                                 else if($reg[$i]['lunch']==0){
                                     echo'<td><input type="checkbox" disabled></td>';
-                                }if($reg[$i]['dinner']==1){
+                                }
+                                
+                                //checks if dinner was eaten
+                                if($reg[$i]['dinner']==1){
                                     echo'<td><input type="checkbox" checked disabled></td>';
                                 }
                                 else if($reg[$i]['dinner']==0){
